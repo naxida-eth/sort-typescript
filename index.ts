eb33681f6623ff03c96906e53902dbc42a5e8544
WuMing <3261937664@qq.com>
2024-04-11 17:20:12 +0800

    选择排序

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { selectSort } from "./src/select-sort";
+
+const data = [
+  1, 654, 4464, 333, 254, 254, 657, 334, 4545, 22226, 6879, 354578, 343434,
+  965874,
+];
+
+selectSort(data);
--- src/select-sort.ts
+++ src/select-sort.ts
@@ -1,19 +1,40 @@
-let data = [
-  1, 654, 4464, 333, 254, 657, 334, 4545, 22226, 6879, 354578, 343434, 965874,
-];
+import { cloneDeep } from "lodash";
 
 // 选择排序的思路是 每次找到最小的数，放到数组的最前面
-let selectSortMin = () => {
-  const sortArr: number[] = [];
-  for (let i = 0; i < data.length; i++) {
-    let min = i;
-    for (let j = i + 1; j < data.length; j++) {
-      if (data[j] < data[min]) {
-        min = j;
-      }
+
+// 找最小的数同时排除掉已经排序过的数
+const findMin = (arr: number[]) => {
+  let minItem = arr[0];
+  let minCurrent = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const indexItem = arr[i];
+    if (minItem >= indexItem) {
+      minItem = indexItem;
+      minCurrent = i;
     }
-    sortArr.push(data[min]);
   }
-  console.log(sortArr);
+
+  return {
+    min: minItem,
+    current: minCurrent,
+  };
 };
-selectSortMin();
+
+const selectSort = (arr: number[]) => {
+  const reArr: number[] = cloneDeep(arr);
+  const sortArr: number[] = [];
+  const minArrCurrent: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    const { min, current } = findMin(reArr);
+    if (typeof min === "number" && typeof current === "number") {
+      minArrCurrent.push(current);
+      sortArr.push(min);
+      reArr.splice(current, 1);
+    }
+  }
+  console.log({
+    sortArr,
+  });
+};
+
+export { selectSort };
